52a950a8c98045e209e2838f7260bf2761ab399c
DewDevth <140059595+DewDevth@users.noreply.github.com>
2023-09-06 08:45:40 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// app.set('trust proxy', true);
+// Define a route that retrieves the client's IP address
+app.get('/get-ip', (req, res) => {
+  const clientIP = req.ip; // This will get the client's IP address
+
+  res.send(`Your IP address is: ${clientIP}`);
+});
+
+// Start the server on port 3000
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
