aa61e088a2bf2479c219c7e2c03d829848acf9f0
DewDevth <140059595+DewDevth@users.noreply.github.com>
2023-09-06 09:14:25 +0700

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 // app.set('trust proxy', true);
 // Define a route that retrieves the client's IP address
-app.get('/get-ip', (req, res) => {
+app.get('/', (req, res) => {
   const clientIP = req.ip; // This will get the client's IP address
 
   res.send(`Your IP address is: ${clientIP}`);
